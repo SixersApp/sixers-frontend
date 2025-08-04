053587fe362969417484ac48fb852b32037a810d
vabhi2007 <vabhi2007@gmail.com>
2025-08-03 18:17:05 -0700

    Revert "feat: add models and services for all tables"
    This reverts commit f7f39c4756796d784869d4737df42631336f1c84.

--- lib/models/player.dart
+++ lib/models/player.dart
@@ -1,19 +1,30 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'tournament.dart';
-import 'real_team.dart';
 
 part 'player.freezed.dart';
 part 'player.g.dart';
 
 @freezed
-class Player with _$Player {
-  @JsonSerializable(fieldRename: FieldRename.snake)
+sealed class Player with _$Player {
   const factory Player({
     required String id,
     required String name,
     required Tournament tournaments,
-    required RealTeam realTeams,
+    required RealTeam real_teams,
   }) = _Player;
 
   factory Player.fromJson(Map<String, dynamic> json) => _$PlayerFromJson(json);
+}
+
+@freezed
+sealed class Tournament with _$Tournament {
+  const factory Tournament({required String id, required String name}) = _Tournament;
+
+  factory Tournament.fromJson(Map<String, dynamic> json) => _$TournamentFromJson(json);
+}
+
+@freezed
+sealed class RealTeam with _$RealTeam {
+  const factory RealTeam({required String id, required String name}) = _RealTeam;
+
+  factory RealTeam.fromJson(Map<String, dynamic> json) => _$RealTeamFromJson(json);
 }
